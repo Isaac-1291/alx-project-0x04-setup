96230b8f9bde7302589e9e740ac4b537ce894497
Isaac-1291 <Profkb91@gmail.com>
2025-08-04 23:28:48 +0000

    alx FE week5 Project

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,39 +1,17 @@
-import Link from "next/link";
-import Button from "@/components/common/Button";
-import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+// components/layouts/Header.tsx
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 const Header: React.FC = () => {
-
-  const pathname = usePathname()
-  const { count } = useCount()
+  const count = useSelector((state: RootState) => state.counter.value);
 
   return (
-    <header className="fixed w-full bg-white shadow-md">
-      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
-          Splash App
-        </Link>
-
-        {/* Button Group */}
-        <div className="flex gap-4">
-          {
-            !["/counter-app"].includes(pathname) ? (
-              <>
-              <Button
-            buttonLabel="Sign In"
-            buttonBackgroundColor="red"
-          />
-          <Button
-            buttonLabel="Sign Up"
-            buttonBackgroundColor="blue"
-          /></>
-            ) : (
-              <p className=" font-semibold text-lg">Current count : {count}</p>
-            )
-          }
-
-        </div>
+    <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">My App</h1>
+      <div className="flex items-center space-x-4">
+        <span className="text-lg">Counter: {count}</span>
       </div>
     </header>
   );
